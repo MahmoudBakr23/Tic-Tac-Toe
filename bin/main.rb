1d9ddf2d310cac4b1cf5e65753c1b6e50db05fc9
Mahmoud Bakr <mbakr6821@gmail.com>
2020-07-16 15:46:04 +0200

    Starting the game flow by adding the player names and board prints

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,23 @@
 #!/usr/bin/env ruby
 
-puts "Hello World!"
+puts "player 1's name ..."
+player_1 = gets.chomp
+puts
+puts "player 2's name ..."
+player_2 = gets.chomp
+puts
+
+puts "#{player_1} is X"
+puts "#{player_2} is O"
+puts
+
+board = [[1,2,3], [4,5,6], [7,8,9]]
+
+(0..board.length - 1).each do |i|
+  (0..board.length - 1).each do |j|
+    print board[i][j].to_s + " "
+  end
+  puts
+end
+puts
+puts "#{player_1}'s move"
