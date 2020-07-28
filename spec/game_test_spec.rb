eb4cf74c232493460446b47d6f38d9a72b0b42be
Mahmoud Bakr <mbakr6821@gmail.com>
2020-07-28 17:16:26 +0200

    add check if win when it's a row

--- spec/game_test_spec.rb
+++ spec/game_test_spec.rb
@@ -1,2 +1,21 @@
-require_relative '../bin/main.rb'
+
 require_relative '../lib/board.rb'
+
+describe Board do
+  let(:board) {Board.new}
+  let(:player_mark) {'X'}
+
+  describe '#win_check' do
+    it "when it's a row" do
+
+      board.mark_on_board(1, player_mark)
+      board.mark_on_board(2, player_mark)
+      board.mark_on_board(3, player_mark)
+
+      expect(board.win_check(player_mark)).to eql(true)
+    end
+
+    it "when it's a column"
+  end
+
+end
