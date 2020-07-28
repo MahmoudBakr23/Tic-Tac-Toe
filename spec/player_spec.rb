a501396111e4b2ab6d652349079c2e6b103e5409
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-28 19:27:20 +0200

    Add tests of the symbols of the player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/player'
+
+describe Player do
+    let(:player_name) { 'tic' }
+    let(:player_mark) { 'X' }
+    let(:player) { Player.new(player_name, player_mark) }
+
+    it 'When a player name is passed' do
+        expect(player.name).to eql(player_name)
+    end
+
+    it 'When a player mark is passed' do
+        expect(player.mark).to eql(player_mark)
+    end
+end
