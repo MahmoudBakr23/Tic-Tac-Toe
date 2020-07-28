8438c4d09aa54b9010bcf2dfdbdecfe2c9f9bf3d
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-28 17:29:01 +0200

    Add the draw test cases

--- spec/game_test_spec.rb
+++ spec/game_test_spec.rb
@@ -41,6 +41,37 @@ describe Board do
 
       expect(board.win_check(player_mark)).to eql(true)
     end
+
+    it "when it's a draw" do
+
+      board.mark_on_board(1, player_mark)
+      board.mark_on_board(2, 'O')
+      board.mark_on_board(3, player_mark)
+      board.mark_on_board(4, 'O')
+      board.mark_on_board(5, player_mark)
+      board.mark_on_board(6, player_mark)
+      board.mark_on_board(7, 'O')
+      board.mark_on_board(8, player_mark)
+      board.mark_on_board(9, 'O')
+
+      expect(board.win_check(player_mark)).to eql(false)
+    end
+
+
+    it "when it's a draw" do
+
+      board.mark_on_board(1, player_mark)
+      board.mark_on_board(2, 'O')
+      board.mark_on_board(3, player_mark)
+      board.mark_on_board(4, 'O')
+      board.mark_on_board(5, player_mark)
+      board.mark_on_board(6, player_mark)
+      board.mark_on_board(7, 'O')
+      board.mark_on_board(8, player_mark)
+      board.mark_on_board(9, 'O')
+
+      expect(board.win_check('O')).to eql(false)
+    end
   end
 
 end
