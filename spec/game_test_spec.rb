677deb883c7c042c84c929bc0a427e64c7772dc8
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-28 17:36:50 +0200

    Fix rubocop errors and fix the tests setup failure

--- spec/game_test_spec.rb
+++ spec/game_test_spec.rb
@@ -1,13 +1,11 @@
-
 require_relative '../lib/board.rb'
 
 describe Board do
-  let(:board) {Board.new}
-  let(:player_mark) {'X'}
+  let(:board) { Board.new }
+  let(:player_mark) { 'X' }
 
   describe '#win_check' do
     it "when it's a row" do
-
       board.mark_on_board(1, player_mark)
       board.mark_on_board(2, player_mark)
       board.mark_on_board(3, player_mark)
@@ -16,7 +14,6 @@ describe Board do
     end
 
     it "when it's a column" do
-      
       board.mark_on_board(1, player_mark)
       board.mark_on_board(4, player_mark)
       board.mark_on_board(7, player_mark)
@@ -25,7 +22,6 @@ describe Board do
     end
 
     it "when it's a diagonal" do
-      
       board.mark_on_board(1, player_mark)
       board.mark_on_board(5, player_mark)
       board.mark_on_board(9, player_mark)
@@ -34,7 +30,6 @@ describe Board do
     end
 
     it "when it's a cross" do
-      
       board.mark_on_board(3, player_mark)
       board.mark_on_board(5, player_mark)
       board.mark_on_board(7, player_mark)
@@ -43,7 +38,6 @@ describe Board do
     end
 
     it "when it's a draw" do
-
       board.mark_on_board(1, player_mark)
       board.mark_on_board(2, 'O')
       board.mark_on_board(3, player_mark)
@@ -57,9 +51,7 @@ describe Board do
       expect(board.win_check(player_mark)).to eql(false)
     end
 
-
     it "when it's a draw" do
-
       board.mark_on_board(1, player_mark)
       board.mark_on_board(2, 'O')
       board.mark_on_board(3, player_mark)
@@ -73,5 +65,4 @@ describe Board do
       expect(board.win_check('O')).to eql(false)
     end
   end
-
 end
